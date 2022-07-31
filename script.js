23906b1fd56950d0ef2a5235f3339565493d9b1b
acopperlily <acopperlily@yahoo.com>
2022-07-31 03:57:51 -0400

    Set up select options

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 console.log('3, 2, 1 ... blastoff!');
-const btn = document.querySelector('button');
+const btn = document.querySelector('input');
 btn.addEventListener('click', getInfo);
 
-function getInfo() {
-  const ship = document.querySelector('input').value;
+async function getInfo(e) {
+  e.preventDefault();
+  const ship = document.querySelector('#fleet').value;
   console.log(ship);
+  try {
+    const res = await fetch(`https://nasa-orbiter-api.herokuapp.com/api/${ship}`);
+    const data = await res.json();
+
+    console.log(data);
+    console.log(data.serialNum);
+    console.log(data.numFlights);
+
+    const h2 = document.querySelector('h2');
+    h2.textContent = `Space Shuttle ${data.name}`;
+    
+  } catch(error) {
+      console.log(error);
+  }
 }
